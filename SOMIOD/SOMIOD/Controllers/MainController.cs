a529327fc28174dd1acca86bb96bc8d5278b9ded
2191802 <82446698+2191802@users.noreply.github.com>
2022-11-06 20:18:13 +0000

    First commit

---
+++ SOMIOD/SOMIOD/Controllers/MainController.cs
@@ -0,0 +1,134 @@
+﻿using SOMIOD.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SOMIOD.Controllers
+{
+    [RoutePrefix("api/somiod")]
+    public class MainController : ApiController
+    {
+        string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["SOMIOD.Properties.Settings.ConnStr"].ConnectionString;
+
+        // !!! Applications !!!
+        // Create
+        [Route("")]
+        public IHttpActionResult PostApplication([FromBody] Application app)
+        {
+            return Ok();
+        }
+
+        // Read
+        [Route("")]
+        public IHttpActionResult GetApplication()
+        {
+            SqlConnection conn = null;
+
+            try
+            {
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                return null;
+            }
+
+            return Ok();
+        }
+
+        // Update
+        [Route("{id:int}")]
+        public IHttpActionResult PutApplication(int id, [FromBody] Application app)
+        {
+            System.Diagnostics.Debug.WriteLine("asd");
+            return Ok();
+        }
+
+        // Delete
+        [Route("{id:int}")]
+        public IHttpActionResult DeleteApplication(int id)
+        {
+            return Ok();
+        }
+
+        // !!! Modules !!!
+        // Create
+        [Route("{module:alpha}")]
+        public IHttpActionResult PostModule([FromBody] Model model)
+        {
+            System.Diagnostics.Debug.WriteLine(model);
+            return Ok();
+        }
+
+        // Read
+        [Route("{module:alpha}")]
+        public IHttpActionResult GetModule(string module)
+        {
+            System.Diagnostics.Debug.WriteLine(module);
+            return Ok();
+        }
+
+        // Update
+        [Route("{module:alpha}/{id:int}")]
+        public IHttpActionResult PutModule(int id, [FromBody] Model model)
+        {
+            System.Diagnostics.Debug.WriteLine("asd");
+            return Ok();
+        }
+
+        // Delete
+        [Route("{module:alpha}/{id:int}")]
+        public IHttpActionResult DeleteModule(int id)
+        {
+            System.Diagnostics.Debug.WriteLine("asd");
+            return Ok();
+        }
+
+        // !!! Data & Subscriptions !!!
+        // Create
+        [Route("{module:alpha}/{value:alpha}")]
+        // value can be Subscription or Data
+        public IHttpActionResult PostSubModule([FromBody] Subscription model)
+        {
+            System.Diagnostics.Debug.WriteLine("asd");
+            return Ok();
+        }
+
+        // Read
+        [Route("{module:alpha}/{value:alpha}")]
+        // value can be Subscription or Data
+        public IHttpActionResult GetSubModule(string module, string value)
+        {
+            System.Diagnostics.Debug.WriteLine(module);
+            System.Diagnostics.Debug.WriteLine(value);
+            return Ok();
+        }
+
+        // Update
+        [Route("{module:alpha}/{value:alpha}/{id:int}")]
+        // value can be Subscription or Data
+        public IHttpActionResult PutSubModule(int id, [FromBody] Subscription model)
+        {
+            System.Diagnostics.Debug.WriteLine("asd");
+            return Ok();
+        }
+        // Delete
+
+        [Route("{module:alpha}/{value:alpha}/{id:int}")]
+        public IHttpActionResult DeleteSubModule(int id)
+        {
+            System.Diagnostics.Debug.WriteLine("asd");
+            return Ok();
+        }
+    }
+}
