62006ea9230afb24cf4d13c633fe73795d587686
Guilherme Lino <lino000000@hotmail.com>
2022-12-12 16:32:35 +0000

    add some stuff

--- SOMIOD/App_B/Form1.Designer.cs
+++ SOMIOD/App_B/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.onButton = new System.Windows.Forms.Button();
+            this.offButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // onButton
+            // 
+            this.onButton.Location = new System.Drawing.Point(106, 83);
+            this.onButton.Name = "onButton";
+            this.onButton.Size = new System.Drawing.Size(179, 43);
+            this.onButton.TabIndex = 0;
+            this.onButton.Text = "ON";
+            this.onButton.UseVisualStyleBackColor = true;
+            this.onButton.Click += new System.EventHandler(this.onButton_Click);
+            // 
+            // offButton
+            // 
+            this.offButton.Location = new System.Drawing.Point(106, 186);
+            this.offButton.Name = "offButton";
+            this.offButton.Size = new System.Drawing.Size(179, 43);
+            this.offButton.TabIndex = 1;
+            this.offButton.Text = "OFF";
+            this.offButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(410, 396);
+            this.Controls.Add(this.offButton);
+            this.Controls.Add(this.onButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button onButton;
+        private System.Windows.Forms.Button offButton;
     }
 }
 
